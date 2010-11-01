5d0d2115985443826910f65a50782235b66613b7
Evan Light <evan.light@tripledogdare.net>
2010-11-01 15:13:42 -0400

    Initial revision

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PickleberryPie::Application.config.secret_token = '6a2b2d2873c86131a3f2a3cc8c1e2ee18340871c9874c9eaaedc72c188e3b4a6614fbe84cc8e7d54063aef93ed89863afce588d4a244b75761ac706c6ba44c1c'
